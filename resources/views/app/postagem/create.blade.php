e1b43c551a8207103179f3c30cdf1c64e0f6f2c3
Fellipev <fellipe2reis@gmail.com>
2024-05-20 20:07:41 -0300

    correção pt

--- resources/views/app/postagem/create.blade.php
+++ resources/views/app/postagem/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div id="create-user-container" class="row text-center mb-3">
         <div class="col-12 justify-content-center menu-titulo">
-            <h1>Criar pesagens!</h1>
+            <h1>Criar postagens!</h1>
         </div>
     </div>
     @component('app.postagem._components.form_create_edit')
